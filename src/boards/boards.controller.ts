21c2a477fdc00ff66db31b715cfa7745863a6364
devgiljong <calf-love@naver.com>
2023-09-14 14:39:13 +0900

    :sparkles: Create board
    add

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,25 @@
+import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardsService } from './boards.service';
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  CreateBoardDto(@Body() createBoardDto: CreateBoardDto) {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
   @Get('/:id')
   getBoardById(@Param('id') id: number) {
     return this.boardsService.getBoardById(id);
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
 import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatus } from './board-status-enum';
 
 @Injectable()
 export class BoardsService {
@@ -9,6 +11,18 @@ export class BoardsService {
     @InjectRepository(BoardRepository)
     private boardRepository: BoardRepository,
   ) {}
+
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const { title, description } = createBoardDto;
+    const board = this.boardRepository.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.boardRepository.save(board);
+    return board;
+  }
 
   async getBoardById(id: number): Promise<Board> {
     const found = await this.boardRepository.findOne(id);
